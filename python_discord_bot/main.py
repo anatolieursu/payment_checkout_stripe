032d10d697e3d7ba09a46786d69b1a0ef40878e2
Ursu Anatolie <104382017+anatolieursu@users.noreply.github.com>
2023-07-28 15:13:23 +0300

    py env and gitignore

--- python_discord_bot/main.py
+++ python_discord_bot/main.py
@@ -1,5 +1,5 @@
-import bot
 from bot import discord_bot
+from env import Token
 
 if __name__ == '__main__':
-    discord_bot('MTEzNDQwMTI0OTY1MjAwMjg2Ng.G8wDyg.kBiMcZxspbyCZrPgrLhGJmx33SZSeXXd6zac_A')
+    discord_bot(Token)
